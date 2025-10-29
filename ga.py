b40056ac1edfcaad0ab66cf9b8726d09210aa75c
abuzar0905 <abuzar0905@gmail.com>
2025-10-29 18:13:11 +0800

    Change TARGET string from 'fakhitah' to 'abu_zar'

--- ga.py
+++ ga.py
@@ -7,7 +7,7 @@ POP_SIZE = 500
 MUT_RATE = 0.2
 
 #TARGET: Our goal.
-TARGET = 'fakhitah'
+TARGET = 'abu_zar'
 
 #GENES: Options from which our population would be created.
 GENES = ' abcdefghijklmnopqrstuvwxyz'
